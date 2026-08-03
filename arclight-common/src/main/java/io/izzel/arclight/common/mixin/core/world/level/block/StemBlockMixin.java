a37727166d1517eac280817c2716cb69a16fef11
root <root@DESKTOP-K420OI7.localdomain>
2026-08-04 02:35:41 +0800

    fix: preserve stem growth event semantics

--- arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/world/level/block/StemBlockMixin.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/world/level/block/StemBlockMixin.java
@@ -1,15 +1,14 @@
 package io.izzel.arclight.common.mixin.core.world.level.block;
 
-import io.izzel.arclight.mixin.Eject;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.block.StemBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import org.bukkit.craftbukkit.v.event.CraftEventFactory;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(StemBlock.class)
 public class StemBlockMixin {
@@ -19,12 +18,18 @@ public class StemBlockMixin {
         return CraftEventFactory.handleBlockGrowEvent(world, pos, newState, flags);
     }
 
-    @Eject(method = "randomTick", at = @At(value = "INVOKE", ordinal = 0, target = "Lnet/minecraft/server/level/ServerLevel;setBlockAndUpdate(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;)Z"))
-    public boolean arclight$cropGrow2(ServerLevel world, BlockPos pos, BlockState state, CallbackInfo ci) {
-        if (!CraftEventFactory.handleBlockGrowEvent(world, pos, state)) {
-            ci.cancel();
-        }
-        return true;
+    @Unique private transient boolean arclight$fruitGrew;
+
+    @Redirect(method = "randomTick", at = @At(value = "INVOKE", ordinal = 0,
+            target = "Lnet/minecraft/server/level/ServerLevel;setBlockAndUpdate(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;)Z"))
+    public boolean arclight$growFruit(ServerLevel world, BlockPos pos, BlockState state) {
+        return this.arclight$fruitGrew = CraftEventFactory.handleBlockGrowEvent(world, pos, state);
+    }
+
+    @Redirect(method = "randomTick", at = @At(value = "INVOKE", ordinal = 1,
+            target = "Lnet/minecraft/server/level/ServerLevel;setBlockAndUpdate(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;)Z"))
+    public boolean arclight$attachStem(ServerLevel world, BlockPos pos, BlockState state) {
+        return this.arclight$fruitGrew && world.setBlockAndUpdate(pos, state);
     }
 
     @Redirect(method = "performBonemeal", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;I)Z"))
