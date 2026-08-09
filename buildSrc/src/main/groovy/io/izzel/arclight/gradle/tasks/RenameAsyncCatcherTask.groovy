9308e664676367bebbceeb748a7b3a09eacf837f
crystalWinter666 <crystalWinter666@163.com>
2026-08-10 04:00:18 +0800

    fix(build): remove Forge mapping cache dependency

--- buildSrc/src/main/groovy/io/izzel/arclight/gradle/tasks/RenameAsyncCatcherTask.groovy
+++ buildSrc/src/main/groovy/io/izzel/arclight/gradle/tasks/RenameAsyncCatcherTask.groovy
@@ -27,7 +27,7 @@ abstract class RenameAsyncCatcherTask extends DefaultTask {
         def input = inputFile.get().asFile
         def output = outputs.files.singleFile
         def tree = new MemoryMappingTree()
-        MappingReader.read(LoomGradleExtension.get(project.project(":arclight-common")).mappingConfiguration.tinyMappingsWithSrg, tree)
+        MappingReader.read(LoomGradleExtension.get(project.project(":arclight-common")).mappingConfiguration.tinyMappings, tree)
         def set = new TinyMappingsReader(tree, "named", mapping.get()).read()
         def map = new JsonSlurper().parse(input) as Map<String, Map<String, String>>
         def mapped = map.collectEntries { ent ->
