8e7dbd1e5ce4192f2bd125feef80c0d2daa67515
crystalWinter666 <crystalWinter666@163.com>
2026-08-10 03:59:34 +0800

    fix(player): avoid legacy init during hand swap

--- arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/server/network/ServerGamePacketListenerImplMixin.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/server/network/ServerGamePacketListenerImplMixin.java
@@ -745,16 +745,8 @@ public abstract class ServerGamePacketListenerImplMixin extends ServerCommonPack
                     if (swapItemsEvent.isCancelled()) {
                         return;
                     }
-                    if (swapItemsEvent.getOffHandItem().equals(offHand)) {
-                        this.player.setItemInHand(InteractionHand.OFF_HAND, originMainHand);
-                    } else {
-                        this.player.setItemInHand(InteractionHand.OFF_HAND, CraftItemStack.asNMSCopy(swapItemsEvent.getOffHandItem()));
-                    }
-                    if (swapItemsEvent.getMainHandItem().equals(mainHand)) {
-                        this.player.setItemInHand(InteractionHand.MAIN_HAND, itemstack);
-                    } else {
-                        this.player.setItemInHand(InteractionHand.MAIN_HAND, CraftItemStack.asNMSCopy(swapItemsEvent.getMainHandItem()));
-                    }
+                    this.player.setItemInHand(InteractionHand.OFF_HAND, CraftItemStack.asNMSCopy(swapItemsEvent.getOffHandItem()));
+                    this.player.setItemInHand(InteractionHand.MAIN_HAND, CraftItemStack.asNMSCopy(swapItemsEvent.getMainHandItem()));
                     this.player.stopUsingItem();
                 }
                 return;
